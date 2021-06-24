3461a761ccdeea00fb0061ed286c5ee88acbd494
angelo1104 <madhav1004@icloud.com>
2021-06-24 05:10:12 +0000

    juniper

---
+++ some/src/supabase.js
@@ -0,0 +1,7 @@
+import {createClient} from "@supabase/supabase-js"
+
+const supabase = createClient('https://supabase.affilboost.com/', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoiYW5vbiJ9.36fUebxgx1mcBo4s19v0SzqmzunP--hm_hep0uLX0ew')
+
+console.log("sup sup", supabase);
+
+export default supabase
